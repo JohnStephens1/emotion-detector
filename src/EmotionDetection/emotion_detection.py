75f4c5b3224654035d0c7e1b0e04f1512afb7fa1
john <johnathan.stephens@protonmail.com>
2025-07-22 19:04:59 +0200

    further clean up emotion_detection.py and add docstrings

--- src/EmotionDetection/emotion_detection.py
+++ src/EmotionDetection/emotion_detection.py
@@ -1,6 +1,8 @@
+"""handles emotion detection logic"""
+
+
 import requests
 import json
-from pprint import pprint
 
 
 URL = r'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -8,6 +10,14 @@ HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_sto
 
 
 def get_post_response(text, url=URL, headers=HEADERS):
+    """
+    posts a request containing the provided text.
+    the url and header default to the relevant emotion prediction model.
+    returns:
+    - the response text, if the request was successful
+    - an empty string otherwise
+    """
+
     text_request = {"raw_document": {"text": text}}
     response = requests.post(url, json=text_request, headers=headers)
 
@@ -17,41 +27,14 @@ def get_post_response(text, url=URL, headers=HEADERS):
         return ""
 
 
-def get_max_emo(le_dict):
-    emo = ""
-    last_val = 0
+def get_emotion_eval(text):
+    """
+    evaluates emotions associated with given string.
+    returns:
+    - the emotion dictionary, associating likelihood with different emotions.
+    - the most likely appropriate emotion as a string
+    """
 
-    for key, val in le_dict.items():
-        if val > last_val:
-            emo = key
-            last_val = val
-        
-    return emo, last_val
-
-
-def emotion_detector(text):
-    response = json.loads(get_post_response(text))
-
-    if not response:
-        return {
-            'anger': None,
-            'disgust': None,
-            'dominant_emotion': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None
-        }
-
-    emotion_dict = response['emotionPredictions'][0]['emotion']
-
-    emo, last_val = get_max_emo(emotion_dict)
-    emotion_dict['dominant_emotion'] = emo
-
-    pprint(emotion_dict)
-    return emotion_dict
-
-
-def mainy_boi(text):
     response_text = get_post_response(text)
 
     if not response_text:
@@ -59,8 +42,6 @@ def mainy_boi(text):
         return ""
 
     emotion_dict = json.loads(response)['emotionPredictions'][0]['emotion']
+    highest_ranking_emotion = max(emotion_dict, key=emotion_dict.get)
 
-    
-    
-emotion_detector("my name eh jeff")
-emotion_detector("")
+    return emotion_dict, highest_ranking_emotion
